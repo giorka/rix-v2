d431dc98b58e312a1c8fe5eed62fffc143e4fb86
giorka <mokenosov@ro.ru>
2024-07-20 12:23:44 +0300

    + FastAPI Application Main

---
+++ src/apps/fastapi/main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from config import settings
+from fastapi import FastAPI, APIRouter
+
+routers: list[APIRouter] = []
+
+
+def get_application() -> FastAPI:
+    application = FastAPI()
+    api_router = APIRouter(prefix='/api')
+
+    for router in routers:
+        api_router.include_router(router)
+
+    application.include_router(api_router)
+
+    return application
+
+
+def get_server(application: FastAPI) -> uvicorn.Server:
+    server = uvicorn.Server(
+        uvicorn.Config(
+            app=application,
+            host='0.0.0.0',
+            port=7000,
+            reload=settings.debug
+        )
+    )
+
+    return server
