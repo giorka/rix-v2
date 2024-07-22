255616825eb8fe13f1515c307bfe9c4de9265eba
giorka <mokenosov@ro.ru>
2024-07-22 15:02:35 +0300

    + User Username Field

--- src/infrastructure/gateways/models/user.py
+++ src/infrastructure/gateways/models/user.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 
 from .base import IModel
 
 
 class UserModel(IModel):
     id = Column(Integer(), primary_key=True)
+    username = Column(String(length=32))
