63b3eee1c71ba29759c82465156a49eec5849ef3
giorka <mokenosov@ro.ru>
2024-07-22 19:21:33 +0300

    + User Data Mapper

---
+++ src/infrastructure/datamappers/user.py
@@ -0,0 +1,10 @@
+from domain.entities import UserEntity
+from ..gateways.models import UserModel
+
+
+def model_to_entity(model: UserModel) -> UserEntity:
+    return UserEntity(username=model.username)
+
+
+def entity_to_model(entity: UserEntity) -> UserModel:
+    return UserModel(**entity.model_dump())
