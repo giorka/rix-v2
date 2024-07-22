c9f94b68706ce41987587db760636cb76bfe6ff5
giorka <mokenosov@ro.ru>
2024-07-22 15:00:30 +0300

    / Alembic ModuleNotFoundError Fixed

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from src.config import settings
-from src.infrastructure.models.base import IModel
+from src.infrastructure.gateways.models.base import IModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
