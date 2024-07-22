af33a472e18848ef57060320f1a78ff125bd89a9
giorka <mokenosov@ro.ru>
2024-07-22 14:02:35 +0300

    + BadSignatureError Exception Handler

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import asyncio
 
+from authlib.jose.errors import BadSignatureError
+from starlette import responses
+
 from applications.fastapi import get_application, get_server
 
 
 async def main() -> None:
     application = get_application()
+
+    @application.exception_handler(BadSignatureError)
+    async def handle_bad_signature_error(*args, **kwargs) -> responses.Response:
+        return responses.JSONResponse(
+            status_code=401,
+            content={'detail': 'Credentials are not provided'},
+        )
 
     await get_server(application).serve()
 
