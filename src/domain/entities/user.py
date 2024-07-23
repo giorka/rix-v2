1ea2f15c328de74aee3c03c6c623a3a70cc48d95
giorka <mokenosov@ro.ru>
2024-07-23 23:32:22 +0300

    - UserCredentialsEntity

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -1,10 +1,5 @@
 from domain import entities, fields
 
 
-class UserCredentialsEntity(entities.IEntity):
-    username: fields.username
-    password: str
-
-
 class UserEntity(entities.IEntity):
     username: fields.username
