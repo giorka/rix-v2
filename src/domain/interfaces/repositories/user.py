b9f4a441454914ec705c5d6e180ce669b82d8f38
giorka <mokenosov@ro.ru>
2024-07-24 21:44:06 +0300

    + UserRepository Get By Username Method

--- src/domain/interfaces/repositories/user.py
+++ src/domain/interfaces/repositories/user.py
@@ -8,3 +8,7 @@ class AbstractUserRepository(AbstractRepository, ABC):
     @abstractmethod
     def create(self, entity: User) -> User:
         raise NotImplementedError()
+
+    @abstractmethod
+    async def get_by_username(self, username: str) -> User:
+        raise NotImplementedError()
--- src/infrastructure/gateways/repositories/alchemy/user.py
+++ src/infrastructure/gateways/repositories/alchemy/user.py
@@ -1,6 +1,9 @@
+from sqlalchemy.future import select
+
 from domain.interfaces.repositories import AbstractUserRepository
 from domain.typevars import User
-from infrastructure.datamappers.user import entity_to_model
+from infrastructure.datamappers.user import entity_to_model, model_to_entity
+from infrastructure.gateways.models import UserModel
 from infrastructure.gateways.repositories.alchemy.base import AlchemyRepository
 
 
@@ -9,3 +12,17 @@ class UserRepository(AlchemyRepository, AbstractUserRepository):
         self.session.add(entity_to_model(entity))
 
         return entity
+
+    async def get_by_username(self, username: str) -> User | None:
+        response = await self.session.execute(
+            select(UserModel)
+            .where(User.username == username)
+        )
+        obj = response.one_or_none()
+
+        if not obj:
+            return None
+
+        model = obj[0]
+
+        return model_to_entity(model)
