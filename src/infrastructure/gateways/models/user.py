e12dfa37c1beeae722e899a967b6da3b1d8d31ec
giorka <mokenosov@ro.ru>
2024-07-25 18:24:03 +0300

    / Alter User Password Length

--- src/infrastructure/gateways/models/user.py
+++ src/infrastructure/gateways/models/user.py
@@ -5,4 +5,4 @@ from .base import IModel
 
 class UserModel(IModel):
     username = Column(String(length=32), primary_key=True)
-    password = Column(String(length=32))
+    password = Column(String(length=128))
