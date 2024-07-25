17832bef185fc63b213fc5a56789e46c760ff4f5
giorka <mokenosov@ro.ru>
2024-07-25 22:34:49 +0300

    - TypeVars (Completely)

--- src/domain/typevars.py
+++
@@ -1,7 +0,0 @@
-from typing import TypeVar
-
-import entities
-from interfaces import repositories
-
-User = TypeVar(bound=entities.UserEntity)
-UserRepository = TypeVar(bound=repositories.AbstractUserRepository)
