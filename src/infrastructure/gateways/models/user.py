b02fdde5ed82b69b4e5f92b9314e34a87ace7c1c
giorka <mokenosov@ro.ru>
2024-10-03 19:57:42 +0300

    refactor(user model): new sqlalchemy syntax
    because of linter's warnings

--- src/infrastructure/gateways/models/user.py
+++ src/infrastructure/gateways/models/user.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, String
-
 from .base import IModel
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import Mapped as M
+from sqlalchemy.orm import mapped_column
 
 
 class UserModel(IModel):
-    username = Column(String(length=32), primary_key=True)
-    password = Column(String(length=128))
+    username: M[str] = mapped_column(String(length=32), primary_key=True)
+    password: M[str] = mapped_column(String(length=128))
