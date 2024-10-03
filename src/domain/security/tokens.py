9a50b550ce274e048233f56c8443d2ca8d1eaaf1
giorka <mokenosov@ro.ru>
2024-10-03 18:31:07 +0300

    refactor(tokens): update imports
    security type aliases is deprecated

--- src/domain/security/tokens.py
+++ src/domain/security/tokens.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass, asdict
 from datetime import timedelta
 from typing import Annotated
+from domain.typealiases import *
 
 from fastapi import Security
 from fastapi_jwt import JwtAccessBearer, JwtAuthorizationCredentials
 
 from config import settings
 from domain.entities import UserEntity
-from .base import Token
 
 jwt = JwtAccessBearer(secret_key=settings.singing_secret, refresh_expires_delta=timedelta(days=7))
 
@@ -21,7 +21,7 @@ def get_user_payload(user: UserEntity) -> Payload:
     return Payload(username=user.username)
 
 
-def make_token(user: UserEntity) -> Token:
+def make_token(user: UserEntity) -> JWT:
     payload = asdict(get_user_payload(user))
 
     return jwt.create_refresh_token(payload)
