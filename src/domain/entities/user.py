09a0078c67f6b285e4967df662e1e046c04ee485
giorka <mokenosov@ro.ru>
2024-10-04 18:59:48 +0300

    refactor(user entity): replace the password data type with alias

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 
+from domain.typealiases import HashedPassword, Password
+
 from . import IEntity
 
 
 @dataclass
 class UserEntity(IEntity):
     username: str
-    password: str | None = None
+    password: Password | HashedPassword
