b9704bebade8bd91251be9dedccbafc23e6a4d7e
giorka <mokenosov@ro.ru>
2024-10-05 00:28:58 +0300

    refactor(use cases): update init
    security base is deprecated

--- src/domain/security/__init__.py
+++ src/domain/security/__init__.py
@@ -1,3 +1,2 @@
-from .base import Password, HashedPassword, Token
-from .passwords import make_password, check_password
-from .tokens import make_token, read_token, Payload, Credentials
+from .passwords import check_password, make_password
+from .tokens import Credentials, Payload, make_token, read_token
