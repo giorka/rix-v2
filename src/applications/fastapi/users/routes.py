9433f81df9d22ede444b5a7a37a2f72ce16ce409
giorka <mokenosov@ro.ru>
2024-10-06 18:32:28 +0300

    feat(user routes): add login route

--- src/applications/fastapi/users/routes.py
+++ src/applications/fastapi/users/routes.py
@@ -27,3 +27,26 @@ async def register(
         raise HTTPException(status_code=409, detail='A user with that username already exists.')
 
     return schemas.LoginUserResponseSchema(token=token)
+
+
+@router.post(
+    '/login',
+    description='Log in as an existing user',
+    status_code=200,
+    responses={
+        200: {'model': schemas.LoginUserRequestSchema, 'description': 'Fine!'},
+    },
+)
+@inject
+async def login(
+        form: schemas.LoginUserRequestSchema,
+        login_user: FromDishka[usecases.LoginUserUseCase],
+):
+    try:
+        token = await login_user(form.username, form.password)
+    except exceptions.UserDoesNotExistException:
+        raise HTTPException(status_code=404, detail='A user with that username does not exists.')
+    except exceptions.IncorrectCredentialsException:
+        raise HTTPException(status_code=401, detail='Credentials are incorrect.')
+
+    return schemas.LoginUserResponseSchema(token=token)
