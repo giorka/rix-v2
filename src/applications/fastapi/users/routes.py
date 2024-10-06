2a2c2c72ab90776d04684a214ea3913f353b6638
giorka <mokenosov@ro.ru>
2024-10-06 18:30:39 +0300

    feat(user routes): add register route

--- src/applications/fastapi/users/routes.py
+++ src/applications/fastapi/users/routes.py
@@ -1,4 +1,29 @@
-from fastapi import APIRouter
+from dishka import FromDishka
+from dishka.integrations.fastapi import inject
+from fastapi import APIRouter, HTTPException
+
+from domain import exceptions, usecases
+from . import schemas
 
 router = APIRouter(prefix='/users')
 
+
+@router.post(
+    '/register',
+    description='Create a new user',
+    status_code=201,
+    responses={
+        201: {'model': schemas.LoginUserResponseSchema, 'description': 'Fine!'},
+    },
+)
+@inject
+async def register(
+        form: schemas.RegisterUserRequestSchema,
+        register_user: FromDishka[usecases.RegisterUserUseCase],
+):
+    try:
+        token = await register_user(form.username, form.password)
+    except exceptions.UserAlreadyExistsException:
+        raise HTTPException(status_code=409, detail='A user with that username already exists.')
+
+    return schemas.LoginUserResponseSchema(token=token)
